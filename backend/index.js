b0f2edb0e1cbde5ff692500480c9973ed09ceeac
Raghav jaiswal <raghavjaiswal0000@gmail.com>
2024-05-28 01:27:52 +0530

    nxt

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,37 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
-const cors = require("cors");
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
+const connectDB = require('./db/connect');
+const userRoutes = require('./routes/user.routes');
+const noteRoutes = require('./routes/note.routes');
 
+dotenv.config();
+connectDB();
 
-const connectDB = require('./db/connect')
+app.use(express.json());
+app.use(cors({
+    origin: ["https://krs-classes-2024-client.vercel.app"],
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    credentials: true
+}));
 
-const userRoutes = require('./routes/user.routes')
-const noteRoutes = require('./routes/note.routes')
+app.use('/users', userRoutes);
+app.use('/notes', noteRoutes);
 
-dotenv.config('')
-
-connectDB()
-
-app.use(express.json())
-app.use(cors(
-    {
-        origin: ["*"],
-        methods: ["POST", "GET","PATCH","DELETE"],
-        credentials: true
-    }
-));
-app.use('/users', userRoutes)
-app.use('/notes', noteRoutes)
-
-app.get("/",(req,res)=>{
-    res.json("hello")
-})
+app.get("/", (req, res) => {
+    res.json("hello");
+});
 
 app.get('/health', (req, res) => {
-    res.json({ message: 'OK' }).status(200)
-})
-app.get('*', (req, res) => {
-    res.json({ error: 'route not found' }).status(404)
-})
+    res.status(200).json({ message: 'OK' });
+});
 
-const PORT = process.env.PORT || 3000
+app.get('*', (req, res) => {
+    res.status(404).json({ error: 'route not found' });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`App has started on PORT: ${PORT}`)
-})
+    console.log(`App has started on PORT: ${PORT}`);
+});
